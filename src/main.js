0799bdc80a56e30bfa1fcfb72a6b4c586826c8b4
戴海洋 <1037306928@qq.com>
2021-01-15 18:53:14 +0800

    fastclick插件以及图片懒加载插件的使用

--- src/main.js
+++ src/main.js
@@ -2,12 +2,24 @@ import Vue from 'vue'
 import App from './App.vue'
 import router from './router'
 import store from './store'
-
-Vue.config.productionTip = false
-
-// 自定义插件导入
+// 插件
 import toast from 'components/common/toast'
+import FastClick from 'fastclick'
+import VueLazyload from 'vue-lazyload'
+
+// 插件安装
 Vue.use(toast)
+
+FastClick.attach(document.body)
+
+Vue.use(VueLazyload, {
+  preLoad: 0.8,
+  attempt: 1
+})
+
+// 创建全局事件总线
+Vue.prototype.$bus = new Vue()
+Vue.config.productionTip = false
 
 new Vue({
   router,
