e9c406b7b655eb697abdc763d575c1d252c9470c
戴海洋 <1037306928@qq.com>
2021-01-15 20:58:39 +0800

    经过postcss-px2viewport插件打包并且将dist文件上传github

---
+++ dist/js/chunk-2d230dc9.2b9dea57.js
@@ -0,0 +1,2 @@
+(window["webpackJsonp"]=window["webpackJsonp"]||[]).push([["chunk-2d230dc9"],{eda5:function(n,e,t){"use strict";t.r(e);var c=function(){var n=this,e=n.$createElement,t=n._self._c||e;return t("div",[n._v(" 分类 ")])},u=[],l={},a=l,r=t("2877"),s=Object(r["a"])(a,c,u,!1,null,null,null);e["default"]=s.exports}}]);
+//# sourceMappingURL=chunk-2d230dc9.2b9dea57.js.map
--- vue.config.js
+++ vue.config.js
@@ -1,10 +1,14 @@
 const path = require('path')
+const pxtovw = require('postcss-px-to-viewport')
+
 function resolve(dir) {
   return path.join(__dirname, dir)
 }
 
 // 配置文件路径的别名----重新设置就需要重新启动一下项目
 module.exports = {
+  publicPath: process.env.NODE_ENV === 'production' ? '/vue_shop/' : '/',
+
   chainWebpack: config => {
     config.resolve.alias
       .set('@', resolve('src'))
@@ -17,9 +21,28 @@ module.exports = {
 
   // 配置sass全局变量
   css: {
+    extract: { ignoreOrder: true },
     loaderOptions: {
       sass: {
         prependData: `@import "~assets/css/var.scss";`
+      },
+      postcss: {
+        //给postcss-loader传递选项
+        plugins: [
+          new pxtovw({
+            unitToConvert: 'px', //需要转换的单位，默认为"px"；
+            viewportWidth: 375, //设计稿的视口宽度
+            unitPrecision: 5, //单位转换后保留的小数位数
+            propList: ['*'], //要进行转换的属性列表,*表示匹配所有,!表示不转换
+            viewportUnit: 'vw', //转换后的视口单位
+            fontViewportUnit: 'vw', //转换后字体使用的视口单位
+            selectorBlackList: [], //不进行转换的css选择器，继续使用原有单位
+            minPixelValue: 1, //设置最小的转换数值
+            mediaQuery: false, //设置媒体查询里的单位是否需要转换单位
+            replace: true, //是否直接更换属性值，而不添加备用属性
+            exclude: [/node_modules/] //忽略某些文件夹下的文件
+          })
+        ]
       }
     }
   }
